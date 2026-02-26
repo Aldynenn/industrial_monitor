75fa119fdfcb65f58aa9308939df6d06e50dfd28
Lázár Marcell <marcell.lazar2003@gmail.com>
2026-02-26 22:34:30 +0100

    Working test verison

--- plc_communication.py
+++ plc_communication.py
@@ -1,6 +1,7 @@
 from tracemalloc import start
 
 import snap7
+import struct
 
 class PLCCommunication:
     connection_error = False
@@ -10,6 +11,7 @@ class PLCCommunication:
         try:
             self.plc.connect(ip_address, rack, slot)
             self.connection_error = self.plc.get_connected()
+            print(f"Connected to PLC: {self.connection_error}")
         except Exception as e:
             print(f"Error connecting to PLC: {e}")
             self.connection_error = True
@@ -17,38 +19,10 @@ class PLCCommunication:
     def disconnect(self):
         self.plc.disconnect()
 
-    def read_boolean(self, db_number, start, size):
-        data = self.plc.db_read(db_number, start, size)
-        return snap7.util.get_bool(data, 0, 0)
-    
-    def write_boolean(self, db_number, start_offset, bit_offset, value):
+    def read_boolean(self, db_number, start_offset, bit_offset):
         reading = self.plc.db_read(db_number, start_offset, 1)
-        snap7.util.set_bool(reading, 0, bit_offset, value)
-        self.plc.db_write(db_number, start_offset, reading)
+        return snap7.util.get_bool(reading, 0, bit_offset)
 
-    def read_integer(self, db_number, start_offset):
-        data = self.plc.db_read(db_number, start_offset, 2)
-        return snap7.util.get_int(data, 0)
-    
-    def write_integer(self, db_number, start_offset, bit_offset, value):
-        data = bytearray(2)
-        snap7.util.set_int(data, 0, value)
-        self.plc.db_write(db_number, start_offset, data)
-
-    def read_real(self, db_number, start_offset):
-        data = self.plc.db_read(db_number, start_offset, 4)
-        return snap7.util.get_real(data, 0)
-    
-    def write_real(self, db_number, start_offset, bit_offset, value):
-        data = bytearray(4)
-        snap7.util.set_real(data, 0, value)
-        self.plc.db_write(db_number, start, data)
-
-    def read_string(self, db_number, start, size):
-        data = self.plc.db_read(db_number, start, size)
-        return snap7.util.get_string(data, 0, size)
-    
-    def write_string(self, db_number, start, value, size):
-        data = bytearray(size)
-        snap7.util.set_string(data, 0, value)
-        self.plc.db_write(db_number, start, data)
+    def read_time(self, db_number, start_offset):
+        reading = self.plc.db_read(db_number, start_offset, 4)
+        return snap7.util.get_dword(reading, 0)
